0ee260dbd475b47b6b089ada1077e7fc51c39d3c
arsalanhub <arsalanmohd237@gmail.com>
2021-10-25 19:09:30 +0530

    created cards and hardcoded values

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,25 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                This is new component
-                <NewsItem />
+            <div className="container my-3">
+                <h2>News Overload Top headlines</h2>
+                <div className="row">
+                    <div className="col-md-3">
+                       <NewsItem title="latest cricket news" description="This news contains latest newas about cricket worls"/>
+                    </div>
+                    <div className="col-md-3">
+                       <NewsItem title="Gadget" description="Samsung launches its latest smartphone which is set to compete with iphone" />
+                    </div>
+                    <div className="col-md-3">
+                       <NewsItem title="Gadget" description="Samsung launches its latest smartphone which is set to compete with iphone" />
+                    </div>
+                    <div className="col-md-3">
+                       <NewsItem title="Gadget" description="Samsung launches its latest smartphone which is set to compete with iphone" />
+                    </div>
+                    <div className="col-md-3">
+                       <NewsItem title="Gadget" description="Samsung launches its latest smartphone which is set to compete with iphone" />
+                    </div>
+                </div>
             </div>
         )
     }
