a39f7b6e2881d4b8e26814abc4bb6ae3557ce02a
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-10-08 23:07:39 +0530

    home route

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ const App = () => {
           <Route exact path="/education" element={<News setProgress={setProgress} key="education" pageSize={pageSize} category="education" />} />
           <Route exact path="/political" element={<News setProgress={setProgress} key="political" pageSize={pageSize} category="political" />} />
           <Route exact path="/cities" element={<News setProgress={setProgress} key="cities" pageSize={pageSize} category="political" />} />
+          <Route exact path="/" element={<News setProgress={setProgress} key="education" pageSize={pageSize} category="education" />} />
         </Routes>
       </Router>
     </div>
